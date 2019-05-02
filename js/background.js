67905fd6267298fc4c5bed3d0b9895e64f38fc6f
JonLuca DeCaro <jdecaro@usc.edu>
2019-05-02 16:35:57 -0700

    'Comments'

--- js/background.js
+++ js/background.js
@@ -1,4 +1,7 @@
-﻿chrome.browserAction.onClicked.addListener(function (tab) {
+﻿/**
+ * Registers the onclick handler for the Chrome Extension icon, injects the content script into a sandboxed chrome extension environment
+ */
+chrome.browserAction.onClicked.addListener(function (tab) {
   chrome.tabs.executeScript(null, {
     file: "js/content_script.js"
   });
--- js/content_script.js
+++ js/content_script.js
@@ -1,8 +1,16 @@
+/**
+ * Function invoked on every load of the script
+ */
 function checkCopyAndPaste() {
   stopPropagationOfType('paste');
   stopPropagationOfType('copy');
 }
 
+/**
+ * Stops propagation of an event to other event listeners, while still allowing the event to complete in its native
+ * context
+ * @param {string} type - the event type, i.e. 'paste', 'keyup', etc
+ */
 function stopPropagationOfType(type) {
   window.addEventListener(type, function (event) {
     event.stopPropagation();
