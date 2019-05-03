fc9467e3b69f18c5547e05461f9b6b8329c5feef
JonLuca DeCaro <jdecaro@usc.edu>
2019-05-03 16:15:31 -0700

    'more overrides'

--- js/content_script.js
+++ js/content_script.js
@@ -4,6 +4,32 @@
 function checkCopyAndPaste() {
   stopPropagationOfType('paste');
   stopPropagationOfType('copy');
+  stopPropagationOfType('contextmenu');
+  stopPropagationOfType('onselectstart');
+  const body = document.getElementsByTagName('body');
+  if (body.length) {
+    addStyle(body[0], 'user-select', 'text', true);
+  }
+}
+
+/**
+ * Adds the given CSS property and value to a DOM element.
+ * @param {HTMLElement} element - element we are adding the CSS property to
+ * @param {string} property - CSS property name, accepts hyphenated form (i.e. user-select rather than userSelect)
+ * @param {string} value - CSS property value
+ * @param {boolean} important - whether to add !important
+ */
+function addStyle(element, property, value, important) {
+  //remove previously defined property
+  if (element.style.setProperty) {
+    element.style.setProperty(property, '');
+  } else {
+    element.style.setAttribute(property, '');
+  }
+
+  //insert the new style with all the old rules
+  element.setAttribute('style', element.style.cssText +
+    property + ':' + value + ((important) ? ' !important' : '') + ';');
 }
 
 /**
